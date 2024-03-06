f44af02ac81bb799a4af898b279143c358ec6d6b
Kevin Cianfarini <kevin.cianfarini@octoenergy.com>
2024-03-05 19:46:00 -0500

    Add missing KDoc (#47)

--- integrations/launch-darkly/src/iosMain/kotlin/io/github/kevincianfarini/monarch/launchdarkly/LaunchDarklyClientShim.kt
+++ integrations/launch-darkly/src/iosMain/kotlin/io/github/kevincianfarini/monarch/launchdarkly/LaunchDarklyClientShim.kt
@@ -8,19 +8,45 @@ package io.github.kevincianfarini.monarch.launchdarkly
  */
 public interface LaunchDarklyClientShim {
 
+    /**
+     * Return a [Boolean] value [forKey], or [default] if no value exists.
+     */
     public fun boolVariation(forKey: String, default: Boolean): Boolean
 
+    /**
+     * Return an [Int] value [forKey], or [default] if no value exists.
+     */
     public fun intVariation(forKey: String, default: Int): Int
 
+    /**
+     * Return a [Double] value [forKey], or [default] if no value exists.
+     */
     public fun doubleVariation(forKey: String, default: Double): Double
 
+    /**
+     * Return a [String] value [forKey], or [default] if no value exists.
+     */
     public fun stringVariation(forKey: String, default: String): String
 
+    /**
+     * Return a JSON encoded [String] value [forKey], or [default] if no value exists.
+     */
     public fun jsonStringVariation(forKey: String, default: String?): String?
 
+    /**
+     * Register a [handler] to be invoked when the value associated with [key] changes, scoped
+     * to [owner].
+     */
     public fun observe(key: String, owner: ObserverOwner, handler: () -> Unit)
 
+    /**
+     * Unregister all observers scoped to [owner].
+     */
     public fun stopObserving(owner: ObserverOwner)
 }
 
-public class ObserverOwner
+/**
+ * A marker object used in [LaunchDarklyClientShim.observe] and
+ * [LaunchDarklyClientShim.stopObserving].
+ */
+public class ObserverOwner internal constructor()
