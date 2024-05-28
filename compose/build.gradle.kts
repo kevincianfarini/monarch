f119e16097a30caa5b6492c540726ed6f127d2e3
Kevin Cianfarini <kevincianfarini@gmail.com>
2024-05-28 17:45:34 -0400

    Migrate to Kotlin 2.0.0 and its built-in Compose plugin

--- compose/build.gradle.kts
+++ compose/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    alias(libs.plugins.compose.plugin)
     alias(libs.plugins.dokka)
+    alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.publish)
 }
