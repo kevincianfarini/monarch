bcddb6a0aa2d57edede54256fdf2a96a1df9b73e
Kevin Cianfarini <kevin.cianfarini@octoenergy.com>
2025-01-17 11:17:55 -0500

    More debug

--- integrations/launch-darkly/build.gradle.kts
+++ integrations/launch-darkly/build.gradle.kts
@@ -56,7 +56,7 @@ private fun KotlinNativeTarget.configureSpmInterop() {
     }
 }
 
-if (Os.isFamily(Os.FAMILY_MAC).also { println("is macOs: $it") }) {
+if (Os.isFamily(Os.FAMILY_MAC).also { error(it) }) {
     swiftPackageConfig {
         create("swiftlaunchdarkly") {
             dependency(
